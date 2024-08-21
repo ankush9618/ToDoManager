944dab3964606039989a2e0bf426b4786e2a328d
ankush9618 <70734844+ankush9618@users.noreply.github.com>
2024-08-22 00:45:39 +0530

    Update useToDoContext.js

--- src/components/useToDoContext.js
+++ src/components/useToDoContext.js
@@ -1,1 +1,23 @@
+import { createContext, useContext } from "react";
+
+export const ToDoContext = createContext({
+    todoItem:[
+        {
+            id:1,
+            todoData:"Learn React",
+            completed:false
+        },
+    ],
+    addItem:(todoData)=>{},
+    deleteItem:(id)=>{},
+    updateItem:(id,todoData)=>{},
+    completedItem:(id)=>{}
+});
+
+export const ToDoProvider = ToDoContext.Provider;
+
+export const useToDo = ()=>{
+    return useContext(ToDoContext);
+}
+
 
