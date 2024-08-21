03c6dd2f2e1b7716cd9da87076d44b7fa5b73c21
ankush9618 <70734844+ankush9618@users.noreply.github.com>
2024-08-22 00:48:00 +0530

    Update AddItems.jsx

--- src/components/AddItems.jsx
+++ src/components/AddItems.jsx
@@ -1,25 +1,23 @@
-import React from "react";
+import React, { useRef } from 'react'
+import { useToDo } from '../components/useToDoContext'
 
-function AddItems({ onChangeHandler, onAddHandler, val }) {
+function AddItems() {
+  const {addItem}=useToDo()
+  const data = useRef("")
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    addItem(data.current.value)
+  }
   return (
     <>
-      <div className="container d-flex justify-content-between">
-        <input
-          type="text"
-          className="flex-grow-1 rounded-start-2 p-2"
-          placeholder="Enter Your Task Here..."
-          onChange={onChangeHandler}
-          value={val}
-        />
-        <button
-          className="bg-primary p-2 rounded-end-2 text-white fw-bold"
-          onClick={onAddHandler}
-        >
-          Add Task
-        </button>
-      </div>
+     <div className='md:w-2/3 w-3/4  mx-auto my-10 rounded-md'>
+      <form action="" className='flex justify-center p-1' onSubmit={submitHandler}>
+        <input ref={data} type="text" placeholder='Enter Your Tasks Here..' className='pl-2 md:w-10/12 w-8/12 p-1 rounded-l-md border border-sky-700'/>
+        <button className='bg-sky-700 text-white font-semibold p-1 md:w-2/12 w-4/12 rounded-r-md'>Add Items</button>
+      </form>
+     </div>
     </>
-  );
+  )
 }
 
-export default AddItems;
+export default AddItems
