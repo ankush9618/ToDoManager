6d9bff42607234b9cbdf089ada831a7dfdce65cf
ankush9618 <70734844+ankush9618@users.noreply.github.com>
2024-08-22 00:49:05 +0530

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='mx-auto text-center text-3xl text-orange-700 font-bold my-10'>
+      ToDo Manager
+    </div>
+  )
+}
+
+export default Header
